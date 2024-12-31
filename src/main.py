6088405cfe0f48d93fc51cbb3007603ee1b69426
nWaldi <ppmip2@gmail.com>
2024-12-31 20:05:07 +0100

    create textnode wchich define text types and has class of textnode, main.py print TextNode instance

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    dummy_TN = TextNode("This is a dummy node", TextType.BOLD, "https://www.boot.dev")
+    print(dummy_TN)
+
+if __name__ == "__main__":
+    main()
