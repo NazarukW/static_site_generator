d3ac840b7d4a525c43f008784f4d00d581fcae76
NazarukW <waldemarnazaruk17@gmail.com>
2025-01-04 20:47:38 +0100

    added __repr__ for leaf and parent

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,6 +33,9 @@ class LeafNode(HtmlNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
+    def __repr__(self):
+        return f"LeafNode(Tag: {self.tag}, value: {self.value}, props: {self.props})"
+
 class ParentNode(HtmlNode):
     def __init__(self, tag: str, children: list, props: dict=None):
         super().__init__(tag, None, children, props)
@@ -43,7 +46,10 @@ class ParentNode(HtmlNode):
         if self.children is None or self.children == []:
             raise ValueError("Invalid HTML: no children")
         node = f"<{self.tag}{self.props_to_html()}>"
-        for item in self.children:
-            node += f"{item.to_html()}"
+        for child in self.children:
+            node += f"{child.to_html()}"
         node += f"</{self.tag}>"
         return node
+
+    def __repr__(self):
+        return f"ParentNode(Tag: {self.tag}, children: {self.children}, props: {self.props})"
